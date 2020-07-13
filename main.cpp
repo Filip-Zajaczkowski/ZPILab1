618fcd91726f83e626e1b3c9ae412b53243a0d31
Filip Zajączkowski <Filip.P.Zajaczkowski@protonmail.com>
2020-07-13 06:52:31 +0200

    Deleted 1, and added 5 lines in main.cpp.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
+    string name;
     cout << "Hello, World!" << endl;
-    cout << "This is the third commit." << endl;
+    cout << "Enter your name.";
+    cin >> name;
+    cout << "My name is "+name+"!" << endl;
     return 0;
 }
